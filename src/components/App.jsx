0bff5f0f311b602293346a513d1f84b2f86e7494
Yurtov <123323348+Yurtov@users.noreply.github.com>
2023-08-10 13:55:01 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Statistics } from './Statistics/Statistics';
+import { ButtonsList, Button } from './App.styled';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  heandleBtnClick = type => {
+    this.setState(prevState => {
+      return { [type]: prevState[type] + 1 };
+    });
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+  
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <p>Please leave feedback</p>
+
+        <ButtonsList>
+          <Button type="button" onClick={() => this.heandleBtnClick('bad')}>
+            Bad
+          </Button>
+          <Button type="button" onClick={() => this.heandleBtnClick('neutral')}>
+            Neutral
+          </Button>
+          <Button type="button" onClick={() => this.heandleBtnClick('good')}>
+            Good
+          </Button>
+        </ButtonsList>
+        <p>Statistics</p>
+        <Statistics good={good} neutral={neutral} bad={bad} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()}/>
+      
+      </div>
+    );
+  }
+}
---
+++ src/components/App.styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ButtonsList = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+export const Button = styled.button`
+ width: 70px;
+ height: 40px;
+ border-radius: 20%;
+ cursor: pointer;
+ font-size: 17px;
+`;
